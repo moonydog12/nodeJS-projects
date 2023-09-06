708c40eb6a9264730a8520314d4322086e04faa7
moonydog12 <losididog@gmail.com>
2023-09-06 22:43:21 +0800

    Add yargs library

--- notes-app/app.ts
+++ notes-app/app.ts
@@ -1,5 +1,55 @@
-import { getNotes } from './note.js'
-import validator from 'validator'
-import chalk from 'chalk'
+import yargs from 'yargs'
 
-const msg = 'success!'
+// Create add command
+yargs.command(
+  'add',
+  'Add a new note',
+  {
+    title: {
+      describe: 'Note title',
+      demand: true,
+      type: 'string',
+    },
+    body: {
+      describe: 'Note body',
+      demand: true,
+      type: 'string',
+    },
+  },
+  // handler function
+  (argv) => {
+    console.log(`Body: ${argv.body}`)
+  },
+)
+
+// Create remove command
+yargs.command(
+  'remove',
+  'Remove a note',
+  () => {},
+  () => {
+    console.log('Removing the note')
+  },
+)
+
+// Create List command
+yargs.command(
+  'list',
+  'list the notes',
+  () => {},
+  () => {
+    console.log('Listing out all notes')
+  },
+)
+
+// Create read command
+yargs.command(
+  'read',
+  'read the notes',
+  () => {},
+  () => {
+    console.log('Reading a note')
+  },
+)
+
+yargs.parse()

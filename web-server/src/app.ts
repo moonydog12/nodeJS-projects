721d47d17e8394f4a535882ed99aac1c9761e702
moonydog12 <losididog@gmail.com>
2023-09-16 00:32:52 +0800

    Setup template engine configuration

--- web-server/src/app.ts
+++ web-server/src/app.ts
@@ -1,22 +1,48 @@
 import express from 'express';
+import path from 'path';
+import hbs from 'hbs';
 
 const app = express();
 const port = 8000;
 
-app.get('', (req, res) => {
-  res.send('Hello test');
-});
+// Define paths for express config in EJS module
+const publicDirectoryPath = path.join(__dirname, '../public');
+const partialsPath = path.join(__dirname, '../templates/partials');
 
-app.get('/help', (req, res) => {
-  res.send('Help page');
+// Setup handlebars engine and views location
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, '../templates/views'));
+hbs.registerPartials(partialsPath);
+
+// Setup static directory
+app.use(express.static(publicDirectoryPath));
+
+app.get('', (req, res) => {
+  res.render('index', {
+    title: 'Weather App',
+    name: 'Moonydog12',
+  });
 });
 
 app.get('/about', (req, res) => {
-  res.send('About');
+  res.render('about', {
+    title: 'About',
+    name: 'Moonydog12',
+  });
+});
+
+app.get('/help', (req, res) => {
+  res.render('help', {
+    title: 'Help',
+    name: 'Moonydog12',
+  });
 });
 
 app.get('/weather', (req, res) => {
-  res.send('Weather page');
+  res.send({
+    location: 'Taiwan',
+    forecast: 'It is raining',
+  });
 });
 
 app.listen(port, () => {
